4762f9915e489614e1644645e89de990595ded65
trent <trent@f4189080-a3fe-f748-8954-90d10a85fca3>
2012-06-08 21:48:50 +0000

    Add icons back

---
+++ icons/ln-lite.sh
@@ -0,0 +1,9 @@
+rm -f *.png
+ln -s "lite/iPadIcon.png" "iPadIcon.png"
+ln -s "lite/Default-Portrait.png" "Default-Portrait.png"
+ln -s "lite/Default.png" "Default.png"
+ln -s "lite/Icon-Small-50.png" "Icon-Small-50.png"
+ln -s "lite/Icon-Small.png" "Icon-Small.png"
+ln -s "lite/Icon@2x.png" "Icon@2x.png"
+ln -s "lite/Icon-Small@2x.png" "Icon-Small@2x.png"
+ln -s "lite/iPhoneIcon.png" "iPhoneIcon.png"
