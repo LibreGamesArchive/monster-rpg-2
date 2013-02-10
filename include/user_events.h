aa22ec0bc187e00e27c3947b3666a069ba1ecc87
Trent Gamblin <trent@nooskewl.com>
2013-02-09 18:32:48 -0700

    Add missing include file, and scaled icons.

---
+++ include/user_events.h
@@ -0,0 +1,12 @@
+#ifndef USER_EVENTS_H
+#define USER_EVENTS_H
+
+enum MyUserEvents {
+	USER_KEY_DOWN = ALLEGRO_GET_EVENT_TYPE('M','R','P','G'),
+	USER_KEY_UP,
+	USER_KEY_CHAR
+};
+
+extern ALLEGRO_EVENT_SOURCE user_event_source;
+
+#endif
