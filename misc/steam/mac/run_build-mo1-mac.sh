411a76938d0d5f05391c7ed4e199cbbd9bfb517c
Trent Gamblin <trent@nooskewl.ca>
2016-07-19 21:43:53 -0600

    Add steampipe for Mac scripts

---
+++ misc/steam/mac/run_build-mo1-mac.sh
@@ -0,0 +1,1 @@
+builder_osx/steamcmd.sh +login trentg MYPASSWORDHERE +run_app_build ../scripts/build-mo1.vdf
---
+++ misc/steam/mac/run_build-mo2-mac.sh
@@ -0,0 +1,1 @@
+builder_osx/steamcmd.sh +login trentg MYPASSWORDHERE +run_app_build ../scripts/build.vdf
