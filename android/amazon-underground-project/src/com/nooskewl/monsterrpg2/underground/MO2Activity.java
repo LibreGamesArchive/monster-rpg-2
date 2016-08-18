0d24a66acf73a0bea34f9437790f6d956d1569f6
Trent Gamblin <trent@nooskewl.ca>
2016-08-18 10:35:44 -0600

    Add Amazon (Android) projects

---
+++ android/amazon-underground-project/src/com/nooskewl/monsterrpg2/underground/MO2Activity.java
@@ -0,0 +1,70 @@
+package com.nooskewl.monsterrpg2.underground;
+
+import org.liballeg.android.AllegroActivity;
+
+import android.net.Uri;
+import android.content.Intent;
+import android.text.ClipboardManager;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import java.io.File;
+import android.view.KeyEvent;
+import android.util.Log;
+import android.app.ActivityManager;
+import android.os.Bundle;
+import java.io.*;
+import android.util.*;
+import java.util.*;
+import java.security.spec.*;
+import android.app.Activity;
+import android.view.View;
+import android.content.IntentFilter;
+
+public class MO2Activity extends AllegroActivity {
+
+   /* load libs */
+   static {
+      System.loadLibrary("allegro_monolith");
+      System.loadLibrary("bass");
+      System.loadLibrary("monsterrpg2");
+   }
+
+	public MO2Activity()
+	{
+		super("libmonsterrpg2.so");
+	}
+
+	public void logString(String s)
+	{
+		Log.d("MoRPG2", s);
+	}
+
+	MyBroadcastReceiver bcr;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		bcr = new MyBroadcastReceiver();
+	}
+	
+	public void onPause() {
+		super.onPause();
+
+		unregisterReceiver(bcr);
+	}
+	
+	public void onResume() {
+		super.onResume();
+
+		registerReceiver(bcr, new IntentFilter("android.intent.action.DREAMING_STARTED"));
+		registerReceiver(bcr, new IntentFilter("android.intent.action.DREAMING_STOPPED"));
+	}
+	
+	public boolean gamepadAlwaysConnected()
+	{
+		return getPackageManager().hasSystemFeature("android.hardware.touchscreen") == false;
+	}
+}
+
